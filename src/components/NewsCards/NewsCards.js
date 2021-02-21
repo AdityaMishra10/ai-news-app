16d17386caebd8f5c021fa948c204e63dfd3841e
Aditya <maditya2000@gmail.com>
2021-02-21 19:34:41 +0530

    initial commit

---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,52 @@
+//cards for home page
+import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
+
+import NewsCard from './NewsCard/NewsCard';
+import useStyles from './styles.js';
+
+const infoCards = [
+  { color: '#24205b', title: 'Latest global news', text: 'Give me the latest news' },
+  { color: '#5d2e2e', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
+  { color: '#601a65', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, delhi, Narendra modi...', text: 'What\'s up with India' },
+  { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+];
+
+const NewsCards = ({ articles, activeArticle }) => {
+  const classes = useStyles();
+
+  if (!articles.length) {
+    return (
+      <Grow in>
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {infoCards.map((infoCard) => (
+            //we are mapping over each card specifically
+            <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
+              <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+                <Typography variant="h5" component="h5">{infoCard.title}</Typography>
+                {infoCard.info ? <Typography variant="h7" component="h7">
+                  <strong>
+                    {infoCard.title.split(' ')[2]}</strong>: <br />{infoCard.info}</Typography> : null}
+                <Typography variant="h6" component="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </Grow>
+    );
+  }
+
+  return (
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+            <NewsCard activeArticle={activeArticle} i={i} article={article} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
+  );
+};
+
+export default NewsCards;
